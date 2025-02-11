6d61af27b5a85102a4a48fbafd0b70c9dab651c4
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2025-02-11 03:37:33 +0330

    adding button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {BrowserModule, provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgIconsModule} from '@ng-icons/core';
+import {
+  heroPlusSmallSolid,
+  heroHomeSolid,
+  heroCogSolid,
+  heroCheckSolid,
+  heroExclamationCircleSolid
+} from '@ng-icons/heroicons/solid';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {NavbarComponent} from './core/components/navbar/navbar.component';
+import {ButtonComponent} from './ui/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons({heroPlusSmallSolid, heroHomeSolid, heroCogSolid, heroCheckSolid, heroExclamationCircleSolid})
   ],
   providers: [
     provideClientHydration(withEventReplay())
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
